c546801b56faf750578580c994f2f0f111b0b780
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-04-11 21:24:22 +1000

    Update Patches.cs to force fullscreen borderless and fix resolution resizing.

--- Spiritfall/Patches.cs
+++ Spiritfall/Patches.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Avrahamy;
 using Avrahamy.GG;
 using HarmonyLib;
@@ -20,17 +19,17 @@ namespace Spiritfall
             // Create a list containing the current screen resolution as the only supported resolution.
             var newList = new List<Vector2Int>
             {
-                new(Display.main.systemWidth, Display.main.systemHeight),
+                new(Display.main.systemWidth, Display.main.systemHeight)
             };
-
+        
             // Update instance properties.
             __instance.supportedResolutions = newList;
-
+        
             // Set the result and return false to skip the original method.
             __result = newList;
             return false;
         }
-
+        
         // This method disables the company logo and enables the loading animation.
         [HarmonyPrefix]
         [HarmonyPatch(typeof(CompanyLogo), nameof(CompanyLogo.Update))]
@@ -40,8 +39,8 @@ namespace Spiritfall
             __instance.loadingAnimation.SetActive(true);
             __instance.enabled = false;
         }
-
-        // Their handling of fullscreen/bordeless etc is funky. This forces fullscreen borderless.
+        
+        // Their handling of fullscreen/bordeless etc is funky. This forces proper fullscreen borderless.
         [HarmonyPrefix]
         [HarmonyPatch(typeof(SettingsMenuController), nameof(SettingsMenuController.OnWindowedModeChanged))]
         [HarmonyPatch(typeof(SettingsMenuController), nameof(SettingsMenuController.OnBorderlessChanged))]
@@ -50,7 +49,7 @@ namespace Spiritfall
             value = true;
         }
 
-        // This method resizes the screen to the current system resolution. This is what removes the black bars.
+        // At 3440x1440 this method in its default state sets my resolution to 3413x1440??
         [HarmonyPrefix]
         [HarmonyPatch(typeof(SettingsModel), nameof(SettingsModel.OnScreenResized))]
         public static bool SettingsModel_OnScreenResized(ref SettingsModel __instance)
@@ -59,7 +58,7 @@ namespace Spiritfall
             var currentResolution = Screen.currentResolution;
             currentResolution.height = Display.main.systemHeight;
             currentResolution.width = Display.main.systemWidth;
-
+        
             // Set the new resolution and return false to skip the original method.
             __instance.SetResolution(currentResolution);
             return false;
